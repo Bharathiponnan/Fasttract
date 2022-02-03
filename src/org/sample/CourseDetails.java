33e05afb64caa762138d1d965e48cb014cd66f1f
Bharathiponnen <bharathiponnen@92@gmail.com>
2022-02-03 13:07:02 +0530

    done

--- src/org/sample/CourseDetails.java
+++ src/org/sample/CourseDetails.java
@@ -1,6 +1,9 @@
 package org.sample;
 
 public class CourseDetails {
+	private void m3() {
+		System.out.println("m3 method created by sundhar");
+	}
 	public static void main(String[] args) {
 		int a=100;
 		System.out.println("a:"+a);
