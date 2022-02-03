ac059cdbc7828052c2f44b9d57287c8fe278f4e9
Bharathiponnen <bharathiponnen@92@gmail.com>
2022-02-03 12:50:27 +0530

    done the work

--- src/org/sample/CourseDetails.java
+++ src/org/sample/CourseDetails.java
@@ -1,6 +1,9 @@
 package org.sample;
 
 public class CourseDetails {
+	private void m2() {
+		System.out.println("m2 is created by denish");
+	}
 	public static void main(String[] args) {
 		int a=100;
 		System.out.println("a:"+a);
